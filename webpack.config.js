1ef294843edfeb6ab1e66a408fc41e76e4039ca8
Jonathan Lloyd <jonathan@thisisjonathan.com>
2017-07-13 21:48:40 +0100

    Switch to absolute paths in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    entry: "./src/MatchboxChip8.js",
+    entry: __dirname + "/src/MatchboxChip8.js",
     output: {
         filename: "MatchboxChip8.js",
-        path: "./dist/js",
+        path: __dirname + "/dist/js",
         library: "MatchboxChip8",
         libraryTarget: "var",
     }
